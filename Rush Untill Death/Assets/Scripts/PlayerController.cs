899f3b388f88359871cd3ceca39c68994bc86cf3
Punarva07 <punarva.kondalkar2021@vitbhopal.ac.in>
2022-12-11 03:34:57 +0530

    New Controller Given

--- Rush Untill Death/Assets/Scripts/PlayerController.cs
+++ Rush Untill Death/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[RequireComponent(typeof(CharacterController))]
-
 public class PlayerController : MonoBehaviour
 {
+
     CharacterController characterController;
     private Vector3 direction;
     private float speed;
@@ -14,18 +12,14 @@ public class PlayerController : MonoBehaviour
     private float gravity = -20;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    public Animator anim;
-    public Transform model;
-
-
-
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     float rotationX = 0;
-
+ 
     [HideInInspector]
     public bool canMove = true;
 
+    
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -42,43 +36,22 @@ public class PlayerController : MonoBehaviour
         float vInput = Input.GetAxis("Vertical");
 
 
-        anim.SetFloat("hSpeed", Mathf.Abs(hInput));
-
-        anim.SetFloat("Speed", Mathf.Abs(vInput));
        
-        anim.SetBool("isGrounded", isGrounded);
         direction.x = hInput * speed;
         direction.z = vInput * speed;
         direction.y += gravity * Time.deltaTime;
         direction = transform.TransformDirection(direction);
-        if (Input.GetKey(KeyCode.D))
-        {
-            anim.SetBool("RWalk", true);
-        }
-        else
-        {
-            anim.SetBool("RWalk", false);
-        }
-        
-        if(Input.GetKey(KeyCode.S))
-        {
-            anim.SetBool("BackRun", true);
-        }
-        else
-        {
-            anim.SetBool("BackRun", false);
-        }
+       
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = 6f;
-            anim.SetBool("Run", true);
+           
         }
         else
         {
             speed = 3f;
-            anim.SetBool("Run", false);
-           
+         
         }
 
 
@@ -87,9 +60,10 @@ public class PlayerController : MonoBehaviour
             if (Input.GetButtonDown("Jump"))
             {
                 direction.y = jumpForce;
-                anim.SetBool("Run", false);
+               
             }
         }
+
 
         characterController.Move(direction * Time.deltaTime);
 
@@ -97,7 +71,7 @@ public class PlayerController : MonoBehaviour
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -60, 30);
+            rotationX = Mathf.Clamp(rotationX, -10, 10);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
